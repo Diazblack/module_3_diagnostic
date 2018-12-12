0c5fd747651be5483d423642c32378d397eeb1ba
Cesar Jolibois <diazcd85@gmail.com>
2018-12-12 09:52:40 -0700

    create search controller WIP on the view

--- spec/features/user_goes_to_root_and_sees_search_interface_spec.rb
+++ spec/features/user_goes_to_root_and_sees_search_interface_spec.rb
@@ -4,15 +4,14 @@ describe "User see search interface " do
   it "should see and a form" do
     # As a user
     user = User.create(name: "Will", email: "Will@wi.com", password: "123454")
-
     # When I visit "/"
     visit "/"
     # And I fill in the search form with 80203 (Note: Use the existing search form)
-    fill_in "Search by zip...", with: "80203"
+    fill_in :q, with: "80203"
 
     # And I click "Locate"
     click_on("Locate")
-
+    
     # Then I should be on page "/search"
     expect(current_path).to eq("/search")
     # Then I should see a list of the 10 closest stations within 6 miles sorted by distance
