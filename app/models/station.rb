61a5bc0539707bc845cc3e02fe28e427ff81b296
Cesar Jolibois <diazcd85@gmail.com>
2018-12-12 10:50:01 -0700

    WIP on the stations method on the StationsFacade

---
+++ app/models/station.rb
@@ -0,0 +1,18 @@
+class Station
+
+  attr_reader @name,
+              @address,
+              @fuel_type,
+              @distance,
+              @access_time
+
+  def initialize(data)
+    require "pry"; binding.pry
+    @name = data[:station_name]
+    @address = data[:street_address]
+    @fuel_type = data[:fuel_type_code]
+    @distance = data[:distance]
+    @access_time = data[:access_days_time]
+  end
+
+end
--- app/models/stations.rb
+++
@@ -1,7 +0,0 @@
-class Station
-  def initialize(data)
-    @name = data[:station_name]
-    @address = data[:station_address]
-    @
-  end
-end
