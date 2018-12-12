fa68eef726caa7fca30d29804f3cca37e43f8b28
Cesar Jolibois <diazcd85@gmail.com>
2018-12-12 09:31:36 -0700

    create User model proceded to create controllers

---
+++ spec/factories/users.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :user do
+    name { "MyString" }
+    email { "MyString@email.com" }
+    password { "101010101" }
+  end
+end
--- spec/features/user_goes_to_root_and_sees_search_interface_spec.rb
+++ spec/features/user_goes_to_root_and_sees_search_interface_spec.rb
@@ -8,13 +8,19 @@ describe "User see search interface " do
     # When I visit "/"
     visit "/"
     # And I fill in the search form with 80203 (Note: Use the existing search form)
-    fill_in:  "form-control", with: "80203"
+    fill_in "Search by zip...", with: "80203"
 
     # And I click "Locate"
-    
+    click_on("Locate")
+
     # Then I should be on page "/search"
+    expect(current_path).to eq("/search")
     # Then I should see a list of the 10 closest stations within 6 miles sorted by distance
+    expect(page).to have_content("Stations")
+
+    expect(page).to have_css("station", 10)
     # And the stations should be limited to Electric and Propane
+
     # And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
   end
 end
