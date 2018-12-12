41501e6014b4a86cf13e49e9de712a0daa62e3b6
Cesar Jolibois <diazcd85@gmail.com>
2018-12-12 10:17:29 -0700

    WIP on the json file from the  NREL api

--- app/facade/stations_facade.rb
+++ app/facade/stations_facade.rb
@@ -3,4 +3,16 @@ class StationsFacade
   def initialize(data)
     @zip = data
   end
+
+  def stations
+    response = conn
+    require "pry"; binding.pry
+  end
+
+  def conn
+    Faraday.new(:url => "'https://developer.nrel.gov/api/alt-fuel-stations.v1?limit=10") do |f|
+      f.headers['X-Api-Key'] = ENV['X-API-KEY']
+      f.adapter Faraday.default_adapter
+    end 
+  end
 end
